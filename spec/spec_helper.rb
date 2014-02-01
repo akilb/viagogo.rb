eed424e9343b18a704434a7ff99a525bce67de8c
Akil Burgess <akilburgess@gmail.com>
2014-02-01 13:32:32 +0000

    Hooked up code coverage with SimpleCov and CoverAlls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,15 @@
+require 'simplecov'
+require 'coveralls'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start do
+  add_filter '/spec/'
+  minimum_coverage(99)
+end
+
 require 'viagogo'
 require 'webmock/rspec'
 
--- viagogo.gemspec
+++ viagogo.gemspec
@@ -29,4 +29,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rspec"
   spec.add_development_dependency "webmock"
   spec.add_development_dependency "timecop"
+  spec.add_development_dependency "coveralls"
+  spec.add_development_dependency "simplecov"
 end
